cda3bbcc1bac638f5a9167fbf6e73ff79708bc5e
Matthew Mullin <mattymullin@Matthews-MBP-2.attlocal.net>
2020-06-08 17:44:50 -0400

    view filled

--- gamepitch_app/views.py
+++ gamepitch_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import DeveloperSerializer, GameSerializer, InvestorSerializer, AppealSerializer
+from .models import Developer, Game, Investor, Appeal
+
+class DeveloperView(viewsets.ModelViewSet):
+    queryset = Developer.objects.all()
+    serializer_class = DeveloperSerializer
+
+
+class GameView(viewsets.ModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+
+
+class InvestorView(viewsets.ModelViewSet):
+    queryset = Investor.objects.all()
+    serializer_class = InvestorSerializer
+
+
+class AppealView(viewsets.ModelViewSet):
+    queryset = Appeal.objects.all()
+    serializer_class = AppealSerializer
