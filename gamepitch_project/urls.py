81d919f858d9eec89e3a3dbfa7883487de95ebc1
Matthew Mullin <mattymullin@Matthews-MBP-2.attlocal.net>
2020-06-08 15:39:54 -0400

    added urls

--- gamepitch_project/urls.py
+++ gamepitch_project/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include, re_path
 from django.contrib import admin
-from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.FrontendAppView.as_view()), #New URL for the index route
+    path('api/v1/', include('gamepitch_app.urls')),
+    re_path(r'^.*$', views.FrontendAppView.as_view()), #Says search Frontend for all other routes, so refreshing doesn't make the deployed page error
 ]
