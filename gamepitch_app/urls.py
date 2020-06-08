45677eb9dd916d08f7ea03ce119cc0cb5fef3e61
Matthew Mullin <mattymullin@Matthews-MBP-2.attlocal.net>
2020-06-08 17:46:53 -0400

    urls added

--- gamepitch_app/urls.py
+++ gamepitch_app/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path, include
+from rest_framework import routers
+
 from . import views
 
+router = routers.DefaultRouter()
+router.register('developers', views.DeveloperView)
+router.register('games', views.GameView)
+router.register('investors', views.InvestorView)
+router.register('appeals', views.AppealView)
+
 urlpatterns = [
-]
+    path('', include(router.urls))]
