a0289b1eeee9fa7a949b22f41eabeb61c068d567
Matthew Mullin <mattymullin@Matthews-MBP-2.attlocal.net>
2020-06-08 17:36:43 -0400

    game serializer

--- gamepitch_app/serializers.py
+++ gamepitch_app/serializers.py
@@ -6,3 +6,21 @@ class DeveloperSerializer(serializers.ModelSerializer):
     class Meta:
         model = Developer
         fields = ('id', 'name', 'image_url', 'bio', 'email', 'facebook')
+
+
+class GameSerializer(serializers.ModelSerializer):
+    developer = DeveloperSerializer(many=True, read_only=True)
+    class Meta:
+        model = Game
+        fields = (
+            'id',
+            'image_url',
+            'description',
+            'genre',
+            'concept_image',
+            'mechanics',
+            'videos',
+            'demo',
+            'games')
+
+
